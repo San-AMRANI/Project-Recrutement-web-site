a27cba3cb5d424f401a68a80f7dca25cea0a1f07
Hassan AMRANI <amranihassan2003@gmail.comgit>
2024-03-08 15:25:22 +0100

    last update

--- Hassan/saveToPdf.js
+++ Hassan/saveToPdf.js
@@ -8,6 +8,37 @@ $(document).ready(function () {
             canvas: true,
         });
     })
-})
+});
+
+const addFormation = document.getElementById('add-formation');
+const removeFormation = document.getElementById('remove-formation');
+
+const formationTab = [];
+
+addFormation.addEventListener('click', function () {
+    for (let i = 0; i < 4; i++) {
+        formationTab[i] = document.getElementById(`formation${i + 1}`);
+        let computedStyle = window.getComputedStyle(formationTab[i]);
+        if (computedStyle.display === 'none') {
+            formationTab[i].style.display = 'block';
+            return;
+        }
+    }
+});
+
+removeFormation.addEventListener('click', function () {
+    for (let i = formationTab.length-1; i >= 0; i--) {
+        let computedStyle = window.getComputedStyle(formationTab[i]);
+        if (computedStyle.display === 'block') {
+            formationTab[i].style.display = 'none';
+            let form = formationTab[i].querySelector('form'); // Assuming there's only one form inside each formation div
+            if (form) {
+                form.reset(); // Reset the form, clearing all input fields
+            }
+            return;
+        }
+    }
+});
+
 
 
