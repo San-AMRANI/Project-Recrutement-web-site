8e1cbb49c44f545b162939dc6a24b83a0926c474
Hassan AMRANI <amranihassan2003@gmail.comgit>
2024-03-11 02:54:00 +0000

    I'm tired :)

--- Hassan/mainprofil.js
+++ Hassan/mainprofil.js
@@ -1,8 +1,37 @@
+$(document).ready(function () {
+    var defaultImage = '../media/utilisateur.png';
+
+    var readURL = function (input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            // Set default image
+            $('.profile-pic').attr('src', defaultImage);
+        }
+    }
+
+    $(".file-upload").on('change', function () {
+        readURL(this);
+    });
+
+    $(".upload-button").on('click', function () {
+        $(".file-upload").click();
+    });
+
+    // Initialize with default image
+    $('.profile-pic').attr('src', defaultImage);
+
+});
+
 document.getElementById('remove-pdf').addEventListener('click', function () {
     var pdfViewer = document.getElementById('pdf-viewer');
     pdfViewer.src = ''; // Reset iframe source
-    pdfViewer.style.display = "none"; // Hide the iframe
-    document.getElementById('pdf-upload').value = ''; // Reset file input
+    document.getElementById('pdf-upload').value = '';
 });
 document.getElementById('pdf-upload').addEventListener('change', function (event) {
     var file = event.target.files[0];
@@ -15,3 +44,4 @@ document.getElementById('pdf-upload').addEventListener('change', function (event
 
     reader.readAsDataURL(file);
 });
+
