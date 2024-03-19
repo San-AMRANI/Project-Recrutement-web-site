c1eb9b957d54829a5b80232660c9cc519c141a5e
wassim boutrasseyt <boutrasseytwassim@gmail.com>
2024-03-19 00:42:33 +0000

    sdsddsqdqdqsdqs

--- jihane/jobApply.sql
+++ jihane/jobApply.sql
@@ -124,7 +124,7 @@ CREATE TABLE message (
 CREATE TABLE photo(
     id INT AUTO_INCREMENT PRIMARY KEY ,
     iduser INT NOT NULL,
-    avatar TEXT , 
+    avatar TEXT ,
     indx INT , 
     FOREIGN KEY (iduser) REFERENCES user(iduser)
 );
